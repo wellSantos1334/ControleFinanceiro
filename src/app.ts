46b8773b48b535cb77d49d99c1e0ef4a420c592f
wellSantos1334 <103972693+wellSantos1334@users.noreply.github.com>
2022-11-14 11:32:21 -0300

    feat: creating dev structure

---
+++ src/app.ts
@@ -0,0 +1,3 @@
+import { server } from './server/Server'
+
+server.listen(process.env.PORT || 3333, () => { console.log(`Server running on ${process.env.PORT || 3333} PORT`) })
---
+++ src/server/Server.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import { router } from './routes/routes'
+import 'dotenv/config'
+
+const server = express()
+
+server.use(express.json())
+server.use(router)
+
+export { server }
